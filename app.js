96ad58a2061c2f4e19b5acc8b96d282b2f447e4d
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-05-11 10:32:32 -0700

    created get routes for page

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+});
 
 app.listen(PORT, () => {
     console.log('Server running on ' + PORT)
-})
+});
