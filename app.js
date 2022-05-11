31a2d79e51fd405fad92c959bfe2779db1beabbe
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-05-11 10:23:04 -0700

    setup server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//jshint esversion:6
+const express = require('express');
+const ejs = require('ejs');
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(PORT, () => {
+    console.log('Server running on ' + PORT)
+})
