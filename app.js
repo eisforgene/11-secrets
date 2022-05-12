dff18505ce0b64e0417cbf7a270c96532b9abd99
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-05-11 21:28:54 -0700

    connected to mongodb through app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
 
 app.get('/', (req, res) => {
     res.render('home')
